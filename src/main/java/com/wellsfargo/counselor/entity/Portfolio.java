b0ca1f9d3c6dfdac86cf314dbf8c98ee6258ba49
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-23 18:54:57 +0530

    Add Portfolio entity with ManyToOne Client and OneToMany Security relationships

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,80 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private String portfolioName;
+
+    @Column(nullable = false)
+    private String createdDate;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Security> securities;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, String portfolioName, String createdDate) {
+        this.client = client;
+        this.portfolioName = portfolioName;
+        this.createdDate = createdDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
